1b6e53411be0277c1d321907c00870f60dfc32d6
ceren <ceren.tuker@smartly.io>
2021-11-29 19:36:58 +0100

    added the paired country city dropdowns

--- flask_wtforms_tutorial/routes.py
+++ flask_wtforms_tutorial/routes.py
@@ -1,7 +1,7 @@
 """Routing."""
 from flask import current_app as app
-from flask import redirect, render_template, url_for
-
+from flask import request, redirect, render_template, url_for, jsonify
+import json
 from .forms import ContactForm, SignupForm
 
 
@@ -14,6 +14,18 @@ def home():
         title="Ad Creator Form App"
     )
 
+@app.route('/getcities', methods=["GET", "POST"])
+def pick_county():
+    try:
+        countryId = request.args.get('countryId', "DEU", type=str)
+        if countryId != '':
+            with open('countries_cities.json') as fp: # todo migrate to a db once app is launched
+                data = json.load(fp)
+            return jsonify(result=data[countryId])
+        else:
+            return jsonify(result=[''])
+    except Exception as e:
+        return str(e)
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
@@ -50,7 +62,6 @@ def comingsoon():
         template="form-template",
         title="Page coming soon"
     )
-
 
 @app.route("/success", methods=["GET", "POST"])
 def success():
