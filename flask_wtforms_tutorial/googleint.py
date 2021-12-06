790e346616883cf6433ffa4b413f7dda6d0051a2
ceren <ceren.tuker@smartly.io>
2021-12-06 19:26:14 +0100

    updated video and gdrive management

--- flask_wtforms_tutorial/googleint.py
+++ flask_wtforms_tutorial/googleint.py
@@ -2,90 +2,113 @@ import gspread
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
+from werkzeug.utils import secure_filename
 from PIL import Image
 from io import BytesIO
 import os
-import subprocess
+import cv2
+import datetime
 
 def write_into_sheet(ad_data):
-    try:
-        gc = gspread.service_account(filename='flask_wtforms_tutorial/service_account.json')
-        sh = gc.open("Facebook Ads Express - Wolt")
-        worksheet = sh.sheet1
-        next_row = next_available_row(worksheet)
-        row_to_begin = "A{}".format(next_row)
-        worksheet.update(row_to_begin, [ad_data])
-        return "ok"
-    except Exception as e:
-        print('An error occurred while writing to the sheet', e)
-        return "error"
+	try:
+		gc = gspread.service_account(
+			filename='flask_wtforms_tutorial/service_account.json')
+		sh = gc.open("Facebook Ads Express - Wolt")
+		worksheet = sh.sheet1
+		next_row = next_available_row(worksheet)
+		row_to_begin = "A{}".format(next_row)
+		worksheet.update(row_to_begin, [ad_data])
+		return "ok"
+	except Exception as e:
+		print('An error occurred while writing to the sheet', e)
+		return "error"
 
 
 def next_available_row(worksheet):
-    str_list = list(filter(None, worksheet.col_values(1)))
-    return str(len(str_list)+1)
+	str_list = list(filter(None, worksheet.col_values(1)))
+	return str(len(str_list)+1)
 
 
-def write_into_drive(filebytes, file):
-    try:    
-        gauth = GoogleAuth()
-        scope = ["https://www.googleapis.com/auth/drive"]
-        gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('flask_wtforms_tutorial/service_account.json', scope)
-        drive = GoogleDrive(gauth)
-        drive_file = drive.CreateFile({'title':file.filename,
-                              'mimeType': file.mimetype,
-        })
+def write_into_drive(filebytes, file, filename):
+	try:
+		gauth = GoogleAuth()
+		scope = ["https://www.googleapis.com/auth/drive"]
+		gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(
+			'flask_wtforms_tutorial/service_account.json', scope)
+		drive = GoogleDrive(gauth)
+		drive_file = drive.CreateFile({'title': filename,
+							  'mimeType': file.mimetype,
+		})
 
-        if not os.path.exists('temp/' + file.filename):
-                image = Image.open(BytesIO(filebytes))
-                image.save('temp/' + file.filename)
-                image.close()
+		if not os.path.exists('temp/' + filename):
+			if (file.mimetype == "image/jpeg" or file.mimetype == "image/png" or file.mimetype == "image/svg"):
+				image = Image.open(BytesIO(filebytes))
+				image.save('temp/' + filename)
+				image.close()
+			else :
+				open('temp/'+filename, 'wb').write(filebytes)
 
-        drive_file.SetContentFile('temp/' + file.filename)
-        drive_file.Upload()
-        #SET PERMISSION todo ceren review this idea
-        drive_file.InsertPermission({
-                                    'type': 'anyone',
-                                    'value': 'anyone',
-                                    'role': 'reader'})
+		drive_file.SetContentFile('temp/'+filename)
+		drive_file.Upload()
+		# SET PERMISSION todo ceren review this idea
+		drive_file.InsertPermission({
+									'type': 'anyone',
+									'value': 'anyone',
+									'role': 'reader'})
 
-        #SHARABLE LINK
-        link=drive_file['alternateLink']
+		# SHARABLE LINK
+		link=drive_file['alternateLink']
 
-        #To use the image in Gsheet we need to modify the link as follows
-        link=drive_file['alternateLink']
-        link=link.split('?')[0]
-        link=link.split('/')[-2]
-        link='https://docs.google.com/uc?export=download&id='+link
-        return link
-    except Exception as e:
-        print('ERROR when uploading to drive:', str(e))
-        return ''
+		# To use the image in Gsheet we need to modify the link as follows
+		link=drive_file['alternateLink']
+		link=link.split('?')[0]
+		link=link.split('/')[-2]
+		link='https://docs.google.com/uc?export=download&id='+link
 
-def get_media_size(filename):
-    try:
-        im = Image.open("temp/"+filename)
+		return link
 
-        print(im.size)
-        print(type(im.size))
+	except Exception as e:
+		print('ERROR when uploading to drive:', str(e))
+		return ''
 
-        w, h = im.size
-        print('width: ', w)
-        print('height:', h)
-        return ("{}x{}").format(w,h)
-    except:
-        print("couldnt get media size for image {}").format(filename)
-        return ""
+def get_image_size(filename):
+	try:
+		im = Image.open("temp/"+filename)
 
+		print(im.size)
+		print(type(im.size))
 
+		w, h = im.size
+		print('width: ', w)
+		print('height:', h)
+		return ("{}x{}").format(w,h)
+	except Exception as e:
+		print("couldn't get media size for image {} with error {}").format(filename, str(e))
+		return ""
+
+def get_video_size(filename):
+	try:
+		vid = cv2.VideoCapture('temp/' + filename)
+		height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+		width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+		return ('{}x{}').format(width,height)
+	except Exception as e:
+		print("couldn't get media size for video {} with error {}").format(filename, str(e))
+		return ""
+
+# https://stackoverflow.com/questions/49048111/how-to-get-the-duration-of-video-using-cv2
+# seek to the end, then get the timestamp to find the length of the stream
 def get_video_duration(filename):
-    try:
-        result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                                 "format=duration", "-of",
-                                 "default=noprint_wrappers=1:nokey=1", 'temp/'+filename],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
-        return float(result.stdout)
-    except:
-        print("couldnt get video length for file {}").format(filename)
-        return ""
+	try:
+		vid = cv2.VideoCapture('temp/' + filename)
+		# count the number of frames
+		frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+		fps = int(vid.get(cv2.CAP_PROP_FPS))
+
+		# calculate dusration of the video
+		seconds = int(frames / fps)
+		video_time = str(datetime.timedelta(seconds=seconds))
+		return video_time
+	except Exception as e:
+		print ("couldn't get video length for file {} with error {}").format(filename, str(e))
+		return ""
--- flask_wtforms_tutorial/routes.py
+++ flask_wtforms_tutorial/routes.py
@@ -3,7 +3,6 @@ from flask import current_app as app
 from flask import request, redirect, render_template, url_for, jsonify
 from .forms import AdForm
 from .googleint import *
-from .data_fetcher import *
 from datetime import datetime, date
 from werkzeug.utils import secure_filename
 import json
@@ -11,113 +10,112 @@ import json
 
 @app.route("/")
 def home():
-    """Landing page."""
-    return render_template(
-        "index.jinja2",
-        template="home-template",
-        title="Ad Creator Form App"
-    )
+	"""Landing page."""
+	return render_template(
+		"index.jinja2",
+		template="home-template",
+		title="Ad Creator Form App"
+	)
 
 @app.route('/getcities', methods=["GET", "POST"])
 def pick_county():
-    try:
-        countryId = request.args.get('countryId', "DEU", type=str)
-        if countryId != '':
-            with open('countries_cities.json') as fp: # todo migrate to a db once app is launched
-                data = json.load(fp)
-            return jsonify(result=data[countryId])
-        else:
-            return jsonify(result=[''])
-    except Exception as e:
-        return str(e)
+	try:
+		countryId = request.args.get('countryId', "DEU", type=str)
+		if countryId != '':
+			with open('countries_cities.json') as fp: # todo migrate to a db once app is launched
+				data = json.load(fp)
+			return jsonify(result=data[countryId])
+		else:
+			return jsonify(result=[''])
+	except Exception as e:
+		return str(e)
 
 @app.route("/createad", methods=["GET", "POST"])
 def createad():
-    """Create ads"""
-    form = AdForm(request.form)
-    print(form['attachment'].data)
-    if form.validate_on_submit():
-        att = request.files.getlist(form.attachment.name)
-        if att:
-            for madia_upload in att: #picture_upload
-                currentfile = att[0]
-                filename = currentfile.filename
-                media_contents = madia_upload.stream.read()
-                
-                # upload file to google drive
-                media_url = write_into_drive(media_contents, currentfile)
-                
-                file_type = "image" if (currentfile.mimetype == "image/jpeg" or currentfile.mimetype == "image/png" or currentfile.mimetype == "image/svg") else "video"
-                video_length = "na" if(file_type == "image" ) else get_video_duration(filename)
-                mediasize = get_image_size(filename) if(file_type == "image" ) else get_video_size(filename)
-                start_date = "" if (form['startdate'].data == date(1111,11,11)) else form['startdate'].data.strftime('%m/%d/%Y')
-                end_date = "" if (form['enddate'].data == date(1111,11,11)) else form['enddate'].data.strftime('%m/%d/%Y')
-                coordinates = get_coordinates(form['city'].data)
-                
-                # [ID,language, adFormat, copy, title, CTA, startDate, endDate, creativeType, conceptName, adName, 
-                # mediaSize, videoLength, mediaURL, country, city, UA or R&F, coordinates, Live, facebook page, IG account]
-                ad_data = [
-                    datetime.now().strftime("%c"),
-                    form['language'].data,
-                    file_type,
-                    form['adcopy'].data,
-                    form['adtitle'].data,
-                    form['calltoaction'].data,
-                    start_date,
-                    end_date,
-                    form['creativetype'].data,
-                    form['creativeconcept'].data,
-                    form['adname'].data,
-                    mediasize, # mediasize
-                    video_length, # video length (na for images)
-                    media_url,
-                    form['country'].data,
-                    form['city'].data,
-                    form['objective'].data, # UA or R&F or both
-                    coordinates # coordinates
-                    # fb page id, filled in the sheet with a vlookup
-                    # instagram id, filled in the sheet with a vlookup
-                ]
-                write_status = write_into_sheet(ad_data)
-                if write_status == 'ok':
-                    return redirect(url_for("success"))
-                else:
-                    return render_template(
-                    "adcreation.jinja2",
-                    form=form,
-                    template="form-template",
-                    title="Create Ad Form"
-                )
-        else:
-            return render_template(
-            "adcreation.jinja2",
-            form=form,
-            template="form-template",
-            title="Create Ad Form"
-        )
-        
+	"""Create ads"""
+	form = AdForm(request.form)
+	print(form['attachment'].data)
+	if form.validate_on_submit():
+		att = request.files.getlist(form.attachment.name)
+		if att:
+			for madia_upload in att:
+				currentfile = att[0]
+				filename = secure_filename(currentfile.filename)
+				media_contents = madia_upload.stream.read()
+				
+				# upload file to google drive
+				media_url = write_into_drive(media_contents, currentfile, filename)
+				
+				file_type = "image" if (currentfile.mimetype == "image/jpeg" or currentfile.mimetype == "image/png" or currentfile.mimetype == "image/svg") else "video"
+				video_length = "na" if(file_type == "image" ) else get_video_duration(filename)
+				mediasize = get_image_size(filename) if(file_type == "image" ) else get_video_size(filename)
+				start_date = "" if (form['startdate'].data == date(1111,11,11)) else form['startdate'].data.strftime('%m/%d/%Y') #encountered what seems to be a bug in validation of empty values in wtforms
+				end_date = "" if (form['enddate'].data == date(1111,11,11)) else form['enddate'].data.strftime('%m/%d/%Y')
+				
+				# remove from local temp file
+				if os.path.exists('temp/' + filename):
+					os.remove('temp/' + filename)
+				
+				ad_data = [
+					datetime.now().strftime("%c"),
+					form['language'].data,
+					file_type,
+					form['adcopy'].data,
+					form['adtitle'].data,
+					form['calltoaction'].data,
+					start_date,
+					end_date,
+					form['creativetype'].data,
+					form['creativeconcept'].data,
+					form['adname'].data,
+					mediasize,
+					video_length,
+					media_url,
+					form['country'].data,
+					form['city'].data,
+					form['objective'].data, # UA or R&F or both
+				]
+				write_status = write_into_sheet(ad_data)
 
-    return render_template(
-        "adcreation.jinja2",
-        form=form,
-        template="form-template",
-        title="Create Ad Form"
-    )
+				if write_status == 'ok':
+					return redirect(url_for("success"))
+				else:
+					return render_template(
+					"adcreation.jinja2",
+					form=form,
+					template="form-template",
+					title="Create Ad Form"
+				)
+		else:
+			return render_template(
+			"adcreation.jinja2",
+			form=form,
+			template="form-template",
+			title="Create Ad Form"
+		)
+		
+
+	return render_template(
+		"adcreation.jinja2",
+		form=form,
+		template="form-template",
+		title="Create Ad Form"
+	)
 
 
 @app.route("/comingsoon", methods=["GET", "POST"])
 def comingsoon():
-    """Coming soon page."""
-    return render_template(
-        "comingsoon.jinja2",
-        template="form-template",
-        title="Page coming soon"
-    )
+	"""Coming soon page."""
+	return render_template(
+		"comingsoon.jinja2",
+		template="form-template",
+		title="Page coming soon"
+	)
 
 @app.route("/success", methods=["GET", "POST"])
 def success():
-    """Generic success page upon form submission."""
-    return render_template(
-        "success.jinja2",
-        template="success-template"
-    )
+	"""Generic success page upon form submission."""
+	return render_template(
+		"success.jinja2",
+		template="success-template"
+	)
